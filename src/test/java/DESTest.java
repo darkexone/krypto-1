a052490cbf43fa9c4307dceccb7b641c1975321e
Hubert Krzemiński <229928@edu.p.lodz.pl>
2020-11-21 15:06:53 +0100

    Implement DESTest

--- src/test/java/DESTest.java
+++ src/test/java/DESTest.java
@@ -1,2 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+
 public class DESTest {
+
+    @Test
+    public void DESTest() {
+
+        String kluczString = "☺☻FF";
+        byte [] klucz;
+
+        System.out.println("Charset: " + Charset.defaultCharset());
+        //klucz = hexToBytes(("7F7F7F7F7F7F7F7F"));
+        //klucz = "☺☻♥♦♣♠•◘".getBytes(Charset.defaultCharset());
+
+        String kluczHexString = String.format("%x", new BigInteger(1, kluczString.getBytes(Charset.defaultCharset())));
+        klucz = GUI.hexToBytes((kluczHexString));
+
+        String tekst = "chujdupaikamienikupa";
+        String tekstHexString = String.format("%x", new BigInteger(1, tekst.getBytes(Charset.defaultCharset())));
+
+        Key kluczyk = new Key(klucz);
+        Subkeys podklucze = new Subkeys(kluczyk);
+        DES DES = new DES(podklucze, GUI.hexToBytes(tekstHexString));
+
+
+    }
 }
